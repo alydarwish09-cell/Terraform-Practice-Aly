f2bf8d40ff9d18771f59442cf55a0c889e2bbe5a
alydarwish09-cell <aly.darwish09@gmail.com>
2025-10-08 09:17:39 +0200

    Exercice 2.2 – Paramétrage avec variables

--- exos/02_docker_nginx/main.tf
+++ exos/02_docker_nginx/main.tf
@@ -1,14 +1,4 @@
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name = "nginx:latest"
-}
-
-resource "docker_container" "nginx" {
-  name  = "nginx_container"
-  image = docker_image.nginx.latest
-  ports {
-    internal = 80
-    external = 8080
-  }
+ports {
+  internal = 80
+  external = var.external_port
 }
